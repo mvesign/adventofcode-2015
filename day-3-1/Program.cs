4d8ba261b7dcbb07ebc9014f1f7c9497c2510310
Maikel Vreugde <git@mvesign.com>
2020-12-29 23:33:20 +0100

    Finalize day 3-1 and start with day 3-2

---
+++ day-3-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 0;
+            var y = 0;
+            var houses = new List<(int, int)>();
+            foreach(var direction in File.ReadAllText("input.txt"))
+            {
+                var house = direction switch
+                {
+                    '^' => (x, y++),
+                    '>' => (x++, y),
+                    'v' => (x, y--),
+                    '<' => (x--, y),
+                    _ => (x, y)
+                };
+
+                if (!houses.Contains(house))
+                    houses.Add(house);
+            }
+
+            Console.WriteLine(houses.Count());
+        }
+    }
+}
