0a67e44c33d11d5a1c447e645ea2f348c3285a8e
Maikel Vreugde <git@mvesign.com>
2020-12-30 22:26:12 +0100

    Day 3-2, this works

--- day-3-2/Program.cs
+++ day-3-2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace day_3_2
 {
@@ -6,7 +9,36 @@ namespace day_3_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var santa = NextHouse(0, 0, ' ');
+            var roboSanta = NextHouse(0, 0, ' ');
+            var santasTurn = false;
+            var houses = new List<(int, int)>();
+            
+            foreach(var direction in File.ReadAllText("input.txt"))
+                if (santasTurn ^= true)
+                    santa = ProcessDirection(santa, direction);
+                else
+                    roboSanta = ProcessDirection(roboSanta, direction);
+
+            Console.WriteLine(houses.Count);
+
+            (int x, int y) NextHouse(int x, int y, char direction) =>
+                direction switch
+                {
+                    '^' => (x, y+1),
+                    '>' => (x+1, y),
+                    'v' => (x, y-1),
+                    '<' => (x-1, y),
+                    _ => (x, y)
+                };
+            
+            (int x, int y) ProcessDirection((int x, int y) current, char direction)
+            {
+                if (!houses.Contains(current))
+                    houses.Add(current);
+
+                return NextHouse(current.x, current.y, direction);
+            }
         }
     }
 }
