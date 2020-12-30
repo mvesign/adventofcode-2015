dcebbcbbb443dd3a970b8c141ba7999298a7fa27
Maikel Vreugde <git@mvesign.com>
2020-12-30 23:16:50 +0100

    Made a start with day 5-1

---
+++ day-5-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vowels = "a e i o u".Split(" ").ToArray();
+            var disallowed = new [] 
+            {
+                "ab", "cd", "pq", "xy"
+            };
+
+            Console.WriteLine(vowels);
+
+            var niceStrings = new List<string>();
+            foreach(var line in File.ReadAllLines("input.txt"))
+            {
+                if (disallowed.Any(line.Contains))
+                    continue;
+
+                if (vowels.Where(line.Contains).Count() < 3)
+                    continue;
+                
+                //TODO It contains at least one letter that appears twice in a row.
+
+                niceStrings.Add(line);
+            }
+
+            Console.WriteLine(niceStrings.Count());
+        }
+    }
+}
