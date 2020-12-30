0848e1be7c7e8279e5404a19116fa91511258580
Maikel Vreugde <git@mvesign.com>
2020-12-30 22:55:08 +0100

    And completed day 4

---
+++ day-4-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace day_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secret = "iwrupvqb";
+
+            using var md5 = MD5.Create();
+
+            for(var count = 0; ; count++)
+                if (
+                    BitConverter.ToString(
+                        md5.ComputeHash(
+                            Encoding.UTF8.GetBytes($"{secret}{count}")
+                        )
+                    )
+                    .Replace("-","")
+                    .StartsWith(new string('0', 5))
+                )
+                {
+                    Console.WriteLine(count);
+                    break;
+                }
+        }
+    }
+}
---
+++ day-4-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace day_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secret = "iwrupvqb";
+
+            using var md5 = MD5.Create();
+
+            for(var count = 0; ; count++)
+                if (
+                    BitConverter.ToString(
+                        md5.ComputeHash(
+                            Encoding.UTF8.GetBytes($"{secret}{count}")
+                        )
+                    )
+                    .Replace("-","")
+                    .StartsWith(new string('0', 6))
+                )
+                {
+                    Console.WriteLine(count);
+                    break;
+                }
+        }
+    }
+}
