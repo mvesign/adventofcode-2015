5bc2adbd21148503612ce42148b5a98c7f1b628b
Maikel Vreugde <git@mvesign.com>
2021-01-03 22:04:32 +0100

    Did made a start with day 6, but not a great one...

---
+++ day-6-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
