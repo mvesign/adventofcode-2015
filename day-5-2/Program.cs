ecc1913ffe5ab8033dce4887e188cfb122999746
Maikel Vreugde <git@mvesign.com>
2021-01-03 19:47:45 +0100

    And finalized day 5-2

---
+++ day-5-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfNice = File.ReadAllLines("input.txt")
+                .Count(line =>
+                    Enumerable.Range(0, line.Length - 1).Any(c => line.IndexOf(line.Substring(c, 2), c + 2) >= 0)
+                    && Enumerable.Range(0, line.Length - 2).Any(c => line[c] == line[c + 2])
+                );
+            
+            Console.WriteLine(numberOfNice);
+        }
+    }
+}
