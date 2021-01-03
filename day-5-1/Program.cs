576edf11b63f5b07d6a3442573b88c9df90ee646
Maikel Vreugde <git@mvesign.com>
2021-01-03 19:30:34 +0100

    Use correct names

--- day-5-1/Program.cs
+++ day-5-1/Program.cs
@@ -15,8 +15,8 @@ namespace day_5_1
             var numberOfNice = File.ReadAllLines("input.txt")
                 .Count(line =>
                     !disallowed.Any(line.Contains)
-                    && line.Count(c => vowels.Contains(c)) >= 3
-                    && Enumerable.Range(0, line.Length - 1).Any(c => line[c] == line[c + 1])
+                    && line.Count(character => vowels.Contains(character)) >= 3
+                    && Enumerable.Range(0, line.Length - 1).Any(character => line[character] == line[character + 1])
                 );
 
             Console.WriteLine(numberOfNice);
