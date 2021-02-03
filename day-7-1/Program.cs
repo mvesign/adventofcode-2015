b4c8267124b57baf3ff4d10142af2dac9255a906
Maikel Vreugde <maikel.vreugde@cm.nl>
2021-02-03 16:28:16 +0100

    Initial setup of day-7-1

---
+++ day-7-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
