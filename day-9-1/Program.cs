4889f20c894010a3136dc4a1abed9d442fca2758
Maikel Vreugde <git@mvesign.com>
2021-03-07 23:07:35 +0100

    The start of day-9-1

---
+++ day-9-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace day_9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string pattern = @"^(\w+) to (\w+) = (\d+)$";
+
+            var routes = File.ReadAllLines("input.txt")
+                .Select(ParseRoute);
+
+            (string from, string to, int distance) ParseRoute(string route)
+            {
+                var match = Regex.Match(instruction, pattern);
+                return (
+                    match.Groups[1].Value,
+                    match.Groups[2].Value,
+                    int.Parse(match.Groups[1].Value)
+                );
+            }
+        }
+    }
+}
