0e9d6bcd4c7028ff36acdece27da32e93739b8c9
Maikel Vreugde <git@mvesign.com>
2021-03-09 23:41:10 +0100

    Not quite there yet

--- day-9-1/Program.cs
+++ day-9-1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace day_9_1
 {
@@ -8,17 +12,68 @@ namespace day_9_1
         {
             const string pattern = @"^(\w+) to (\w+) = (\d+)$";
 
-            var routes = File.ReadAllLines("input.txt")
-                .Select(ParseRoute);
+            var routes = File.ReadAllLines("input.txt").Select(ParseRoute);
+            var cities = GetCities(routes).Distinct().ToArray();
+            var permutations = GetPermutations(cities, 0, cities.Length - 1);
 
-            (string from, string to, int distance) ParseRoute(string route)
+            var distance = permutations.Select(p => GetDistance(p, routes)).Min();
+
+            Console.WriteLine(distance);
+
+            Tuple<string, string, int> ParseRoute(string route)
             {
-                var match = Regex.Match(instruction, pattern);
-                return (
+                var match = Regex.Match(route, pattern);
+                return Tuple.Create(
                     match.Groups[1].Value,
                     match.Groups[2].Value,
-                    int.Parse(match.Groups[1].Value)
+                    int.Parse(match.Groups[3].Value)
                 );
+            }
+
+            IEnumerable<string> GetCities(IEnumerable<Tuple<string, string, int>> routes)
+            {
+                foreach (var (from, to, distance) in routes)
+                {
+                    yield return from;
+                    yield return to;
+                }
+            }
+
+            List<string[]> GetPermutations(string[] cities, int start, int end)
+            {
+                var result = new List<string[]>();
+                if (start == end)
+                    result.Add(cities);
+                else
+                    for(var i = start; i <= end; i++)
+                    {
+                        Swap(ref cities[start], ref cities[i]);
+                        result = result.Union(
+                                GetPermutations(cities, start + 1, end)
+                            )
+                            .ToList();
+                        Swap(ref cities[start], ref cities[i]);
+                    }
+
+                return result;
+            }
+
+            void Swap(ref string a, ref string b)
+            {
+                var temp = a;
+                a = b;
+                b = temp;
+            }
+
+            int GetDistance(string[] cities, IEnumerable<Tuple<string, string, int>> routes)
+            {
+                var distance = 0;
+                for(var i = 0; i < cities.Length - 1; i++)
+                    distance += routes.Where(r => r.Item1 == cities[i] && r.Item2 == cities[i+1])
+                        .Select(r => r.Item3)
+                        .Single();
+
+                return distance;
             }
         }
     }
